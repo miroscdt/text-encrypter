698cabca5c7a8b39e6f486c4a4e987030b7b6d39
miros <miroscdt@gmail.com>
2023-06-07 18:27:04 -0600

    fix: agregué boton copiar

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
 
-function btnEncriptar(){
+
+function btnEncriptar() {
     const textoEncriptado = encriptar(textArea.value);
     mensaje.value = textoEncriptado;
     textArea.value = "";
     mensaje.style.backgroundImage = "none";
 
 }
-function encriptar(stringEncriptado){
-    let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+function encriptar(stringEncriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringEncriptado = stringEncriptado.toLowerCase();
 
-    for (let i = 0; i< matrizCodigo.length; i++) {
+    for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringEncriptado.includes(matrizCodigo[i][0])) {
             stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
@@ -21,20 +22,39 @@ function encriptar(stringEncriptado){
 }
 
 
-function btnDesencriptar(){
+function btnDesencriptar() {
     const textoDesencriptado = desencriptar(textArea.value);
     mensaje.value = textoDesencriptado;
     textArea.value = "";
 }
-function desencriptar(stringDesencriptado){
-    let matrizCodigo = [["e","enter"], ["i","imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+function desencriptar(stringDesencriptado) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringDesencriptado = stringDesencriptado.toLowerCase();
 
-    for (let i = 0; i< matrizCodigo.length; i++) {
+    for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringDesencriptado.includes(matrizCodigo[i][1])) {
             stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
         }
     }
     return stringDesencriptado;
 }
+
+function copyToClipboard() {
+    const textToCopy = document.getElementById('textToCopy').value;
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+            alert('¡Texto copiado!');
+            botonCopiar.classList.remove('clicked');
+        })
+        .catch((error) => {
+            console.error('Error al copiar el texto:', error);
+        });
+}
+
+
+const botonCopiar = document.getElementById('botonCopiar');
+
+botonCopiar.addEventListener('click', () => {
+    botonCopiar.classList.add('clicked');
+});
 
